443da0360591eb7179d4dada14fc858d5fc4374e
Michael Heimark <mh@radrocket.dk>
2018-05-30 11:15:16 +0200

    init

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} : ${req.url}`;
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to append to file.');
+    }
+  });
+  next();
+});
+app.use((req,res,next) => {
+  if(true){
+    res.render('maintenance.hbs', {
+      error: 'There was a database error.'
+    });
+  }
+});
+app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+
+app.get('/', (request, response) => {
+  response.render('index.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome sucka'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page'
+  });
+});
+
+app.get('/bad', (req,res) => {
+  res.send({
+    error: 'Error msg'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port: 3000');
+});
